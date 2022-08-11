90b68b4609e20b5ed22c5332d338889d0bc724db
Alex Strugacz <alexstrugacz@icloud.com>
2022-08-11 16:59:40 -0500

    complete all challenges

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, remove) {
+  for (i = 0; (i = array.length); i++) {
+    if (array[i] === remove) {
+      return array.splice(i, 1);
+    } else continue;
+  }
 };
 
 // Do not edit below this line
