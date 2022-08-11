f09f417d265877739cacb48cfc256ad21baa2104
Alex Strugacz <alexstrugacz@icloud.com>
2022-08-11 14:53:46 -0500

    complete challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let splitWord = word.split("");
+  let reverseArray = splitWord.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line
